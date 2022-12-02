5a41505295d830626b0e9782727c7b013a15dc73
Snayt1k3 <snayt1k3twitch@gmail.com>
2022-12-02 20:53:01 +0300

    add main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import logging
+from Keyboards import kb
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from cfg import token
+from handlers import accountant_hand
+
+API_TOKEN = token
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+storage = MemoryStorage()
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot, storage=storage)
+
+accountant_hand.register_handlers(dp)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi i'm accountant bot", reply_markup=kb)
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    # old style:
+    # await bot.send_message(message.chat.id, message.text)
+
+    await message.answer(message.text)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
