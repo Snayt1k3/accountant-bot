5c810ed692a325accb8c86a1b789aa3e7705805f
Snayt1k3 <snayt1k3twitch@gmail.com>
2022-12-03 21:19:34 +0300

    add fsm

--- handlers/expenses.py
+++ handlers/expenses.py
@@ -1,7 +1,34 @@
 import sqlite3
 from datetime import date
+
 from aiogram import Dispatcher
 from aiogram import types
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
+
+
+class Date(StatesGroup):
+    day = State()
+    month = State()
+
+
+async def start_fsm_expenses(message: types.Message):
+    await Date.day.set()
+    await message.reply("Напиши в какой день,\nхочешь увидеть свои траты")
+
+
+# Принимаем День
+async def process_day(message: types.Message, state: FSMContext):
+    await state.update_data(day=int(message.text))
+    await message.reply("Напиши месяц или укажи .")
+    await Date.next()
+
+
+# Принимаем Месяц
+async def process_month(message: types.Message, state: FSMContext):
+    await state.update_data(month=int(message.text))
+    await message.reply(await all_expenses(message))
+    await state.finish()
 
 
 async def all_expenses(message: types.Message):
@@ -15,9 +42,10 @@ async def all_expenses(message: types.Message):
         if number[0]:
             s += number[0]
 
-    await message.reply(f"Вот ваши траты за {period}\n{s}")
+    return f"Вот ваши траты за {period}\n{s}"
 
 
 def register_handlers(dp: Dispatcher):
-    dp.register_message_handler(all_expenses, commands=['Траты'])
-    
+    dp.register_message_handler(start_fsm_expenses, commands=['Траты'])
+    dp.register_message_handler(process_day, state=Date.day)
+    dp.register_message_handler(process_month, state=Date.month)
